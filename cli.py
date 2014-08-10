811e478c2704183a5d49bdb83f371f15f39e8647
Grant Paton-Simpson <grantps@catalyst.net.nz>
2014-08-10 16:41:00 +1200

    CLI code is now separated from core code.

---
+++ cli.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3.4
+
+"""
+CLI interface to game.
+"""
+
+from time import sleep
+
+import core
+
+if __name__ == '__main__':
+    side_len = 8
+    coordinates = core.get_coordinates(side_len)
+    bb = core.BlackBox(coordinates, side_len)
+    while True:
+        number = input("Enter your number: ")
+        if not number:
+            break
+        try:
+            number = int(number)
+        except ValueError:
+            print("Pick a number between 1 and {}.".format(side_len * 4))
+            continue
+        if number < 1 or number > side_len * 4:
+            print("Pick a number between 1 and {}.".format(side_len * 4))
+            continue
+        print("{} goes to {}.".format(number, bb.exit_ray(number)))
+    print("The coordinates were:")
+    for coordinate in coordinates:
+        sleep(1)
+        print(coordinate)
